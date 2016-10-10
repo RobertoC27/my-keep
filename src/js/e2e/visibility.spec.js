cb36db3b8b4321ae9edfe6faa44429d6628eee49
RobertoC27 <chi13027@uvg.edu.gt>
2016-10-09 18:44:32 -0600

    added unit test

--- src/js/e2e/visibility.spec.js
+++ src/js/e2e/visibility.spec.js
@@ -1,24 +1,50 @@
 import visibilityFilter from '../reducers/visibility';
 
-const testSetVisibilityFilter = () => {
-  const stateBefore = 'ALL';
 
-  const action = {
+const stateBefore = {
+  visibilityFilter: 'SHOW_ALL',
+  search : ''
+}
+
+const testSetVisibilityFilter = () => {
+  const action =  {
     type: 'SET_VISIBILITY_FILTER',
     payload: {
-      visibilityFilter: 'HIDE_COMPLETED'
+      visibilityFilter: 'SHOW_NOTES'
     }
   }
 
-  const stateAfter = 'HIDE_COMPLETED';
+  const stateAfter = {
+    visibilityFilter: 'SHOW_NOTES',
+    search: ''
+  }
 
   deepFreeze(stateBefore);
   deepFreeze(action);
-
-  expect(
-    visibilityFilter(stateBefore, action)
-  ).toEqual(stateAfter);
+  expect(visibilityFilter(stateBefore, action)).toEqual(stateAfter);
 }
 
-testToggleTodo();
-console.log("All visibility filter tests passed!");
+testSetVisibilityFilter();
+console.log('passed SET_VISIBILITY_FILTER')
+
+const testSetSearch = () => {
+  const action =  {
+    type: 'SET_SEARCH',
+    payload: {
+      search: 'buenas'
+    }
+  }
+
+  const stateAfter = {
+    visibilityFilter: 'SHOW_ALL',
+    search: 'buenas'
+  }
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+  console.log(visibilityFilter(stateBefore, action));
+  expect(visibilityFilter(stateBefore, action)).toEqual(stateAfter);
+}
+
+testSetSearch();
+console.log('passed SET_SEARCH')
